35bf88ed6451a2c2c9174f73471fb39a45ee0015
Siddharth Kumar <154543665+siddharth23k@users.noreply.github.com>
2025-09-27 22:04:17 +0530

    remove logging setup
    Remove logging setup and AutoMLBase class.

--- automl/src/__init__.py
+++ automl/src/__init__.py
@@ -1,9 +1,1 @@
-import logging
 
-class AutoMLBase:
-    def __init__(self):
-        self.logger = logging.getLogger("AutoML")
-        logging.basicConfig(level=logging.INFO)
-
-    def log(self, msg):
-        self.logger.info(msg)
